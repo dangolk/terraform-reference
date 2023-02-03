cee305c718a7891376ba849c70682b834fface2f
Kiran Dangol <kiran_dan2009@yahoo.com>
2023-02-03 00:25:12 -0600

    Update main.tf

--- learn-terraform-aws-instance/main.tf
+++ learn-terraform-aws-instance/main.tf
@@ -11,11 +11,11 @@ terraform {
 }
 
 provider "aws" {
-  region = "us-west-2"
+  region = "us-east-1"
 }
 
 resource "aws_instance" "app_server" {
-  ami           = "ami-830c94e3"
+  ami           = "ami-0aa7d40eeae50c9a9"
   instance_type = "t2.micro"
 
   tags = {
